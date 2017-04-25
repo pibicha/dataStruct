4988b8b80025084c3abacd3a6d3163d97ac95446
pbx <pibicha@163.com>
2017-04-25 22:49:34 +0800

    # first commit

---
+++ queue/Queue.py
@@ -0,0 +1,19 @@
+"""
+python中的Queue更像是java的LinkedBlokingQueue;
+这里用的Queue功能更单一一些，只需要先进先出的功能即可；
+内部使用List做容器；
+"""
+
+
+class Queue:
+    def __init__(self):
+        self.item = []
+
+    def enqueue(self, item):
+        self.item.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
