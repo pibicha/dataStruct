85f9538a860d74db175cf98b89a80cd0376834ec
pbx <pibicha@163.com>
2017-04-27 23:40:06 +0800

    ### BinHeap -- delTop & down

--- tree/BinHeap.py
+++ tree/BinHeap.py
@@ -30,18 +30,51 @@ class BinHeap:
                 self.heapList[index], self.heapList[index // 2] = self.heapList[index // 2], self.heapList[index]
             index = index // 2
 
-    def __str__(self):
+    """
+    由于列表中第二个元素就是最小值(小顶堆)，所以删除最小值，只需要
+    删除[1]元素；
+    对于缺少顶部的堆，需要找到列表中最小的一个值来做堆顶，但是就算用
+    找到最小值，将其作为堆顶，也会导致其原先所在的节点不平衡；
+    这里暂时没想到其他的什么方法，能重建堆的方法：
+    1) 将数组的最后一个值挪到[2]上
+    2) 递归处理新堆顶是否需要和子节点交换
+    """
+
+    def popTop(self):
+        # 1)
+        top = self.heapList.pop(1)
+        self.heapList.insert(1, self.heapList.pop())
+        self.currentSize -= 1
+        # 2)
+        self.down(1)
+
+    def down(self, index):
+        while index * 2 <= self.currentSize:
+            # 2.1) 节点是否与子节点交换位子，其实只要和较小的一个子节点比较即可"""
+            minIndex = self.minChildIndex(index)
+            if self.heapList[index] > self.heapList[minIndex]:
+                self.heapList[index], self.heapList[minIndex] = self.heapList[minIndex], self.heapList[index]
+            index = minIndex
+
+    def minChildIndex(self, index):
+        if index * 2 + 1 > self.currentSize:
+            return index * 2
+        else:
+            if self.heapList[index * 2] < self.heapList[index * 2 + 1]:
+                return index * 2
+            else:
+                return index * 2 + 1
+
+    def __repr__(self):
         print(self.heapList)
 
 
-heap = BinHeap()
-heap.insert(3)
-heap.insert(2)
-heap.insert(1)
-heap.insert(6)
-heap.insert(0)
-heap.insert(4)
-heap.insert(7)
-heap.insert(5)
-
-print(heap)
+bh = BinHeap()
+bh.insert(7)
+bh.insert(9)
+bh.insert(11)
+bh.insert(5)
+bh.insert(8)
+# print(bh)
+bh.popTop()
+print(bh)
